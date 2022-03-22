d74f6d23e35a298fb693dcfd183b16beadb7f6aa
Joakim Olsson <joakim.olsson@adicitus.cloud>
2022-03-22 19:49:58 +0100

    Changed the default logger.
    The default logger should hopefully always be able to create a log file
    now.

--- source/.bootstrap.ps1
+++ source/.bootstrap.ps1
@@ -5,25 +5,40 @@ function _buildBasicFileLogger {
 
     return {
         param($message)
-        $message | Out-File $FilePath -Encoding utf8 -Append
+
+        if (-not (Test-Path $FilePath -PathType Leaf)) {
+            New-Item -Path $FilePath -ItemType File -Force -ErrorAction Stop | Out-Null
+        }
+
+        $message | Out-File $FilePath -Encoding utf8 -Append -Force
     }.GetNewClosure()
 }
 
 
-if ( !(Get-variable "_ShoutOutSettings" -ErrorAction SilentlyContinue) -or $script:_ShoutOutSettings -isnot [hashtable]) {
-    $script:_ShoutOutSettings = @{
-        DefaultMsgType="Info"
-        DefaultLog=_buildBasicFileLogger ("C:\temp\shoutOut.{0}.{1}.{2:yyyyMMddHHmmss}.log" -f $env:COMPUTERNAME, $pid, [datetime]::Now)
-        LogFileRedirection=@{}
-        MsgStyles=@{
-            Success =       @{ ForegroundColor="Green" }
-            Exception =     @{ ForegroundColor="Red"; BackgroundColor="Black" }
-            Error =         @{ ForegroundColor="Red" }
-            Warning =       @{ ForegroundColor="Yellow"; BackgroundColor="Black" }
-            Info =          @{ ForegroundColor="Cyan" }
-            Result =        @{ ForegroundColor="White" }
-        }
-        LogContext=$true
-        Disabled=$false
+$script:_ShoutOutSettings = @{
+    DefaultMsgType="Info"
+    LogFileRedirection=@{}
+    MsgStyles=@{
+        Success =       @{ ForegroundColor="Green" }
+        Exception =     @{ ForegroundColor="Red"; BackgroundColor="Black" }
+        Error =         @{ ForegroundColor="Red" }
+        Warning =       @{ ForegroundColor="Yellow"; BackgroundColor="Black" }
+        Info =          @{ ForegroundColor="Cyan" }
+        Result =        @{ ForegroundColor="White" }
+    }
+    LogContext=$true
+    Disabled=$false
+}
+
+$defaultLogFilename = "{0}.{1}.{2:yyyyMMddHHmmss}.log" -f $env:COMPUTERNAME, $pid, [datetime]::Now
+
+$defaultLogFile = switch ((whoami).split('\')[0]) {
+    System  {
+        "{0}\Logs\shoutOut\{1}" -f $env:windir, $defaultLogFilename
+    }   
+    default {
+        "{0}\shoutOut\Logs\{1}" -f $env:APPDATA, $defaultLogFilename
     }
 }
+
+$script:_ShoutOutSettings.DefaultLog = _buildBasicFileLogger $defaultLogFile
